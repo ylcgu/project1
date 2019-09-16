b9eae9a5e69be186fcd72837ef48d1758f63592a
ylcgu <54045235+ylcgu@users.noreply.github.com>
2019-09-16 18:11:24 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import pandas as pd
 
 ########### Define your variables ######
 myheading = "AirBnbs in Mahanttan is very expensive!!!"
-mysubheading = "Data in 2017"
 tabtitle = 'AirBnb Price by Areas in New York City'
 filename = 'CleanedData.csv'
 sourceurl = 'https://www.kaggle.com/dgomonov/new-york-city-airbnb-open-data/'
@@ -79,11 +78,23 @@ fig=go.Figure(data=[mydata2,mydata2,mydata3], layout=mylayout2)
 iplot(fig)
 
 ########### Initiate the app
-########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.title=tabtitle
+
+########### Set up the layout
+app.layout = html.Div(children=[
+    html.H1(myheading),
+    dcc.Graph(
+        id='figure-1',
+        figure=fig
+    ),
+    html.A('Code on Github', href=githublink),
+    html.Br(),
+    html.A("Data Source", href=sourceurl),
+    ]
+)
 
 ############ Deploy
 if __name__ == '__main__':
